35e5b092812dddd937c2fec7e3098311da2e05b2
playerthomasm6 <69253499+playerthomasm6@users.noreply.github.com>
2021-01-17 14:16:49 -0500

    file setup and workoutModel.js

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+const db = require("./models");
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/custommethoddb", { useNewUrlParser: true });
+
+
+
+
+app.post("/exercise", ({body}, res) => {
+  const user = new Workout(body);
+  user.setFullName();
+  user.lastUpdatedDate();
+
+  User.create(user)
+    .then(dbUser => {
+      res.json(dbUser);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
+
